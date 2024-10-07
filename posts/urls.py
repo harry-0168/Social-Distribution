9a99165d05b9877b2d1d0a05bbe423838a659158
sharon2d24911 <sharon2d24911@gmail.com>
2024-10-06 21:00:40 -0600

    implemented the ui

---
+++ posts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.CreatePostView.as_view(), name="index"),
+]
--- socialDistribution/settings.py
+++ socialDistribution/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'author',
+    'posts',
+    'inbox',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
