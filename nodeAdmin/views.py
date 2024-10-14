4dd8c2c0dee331e19c2a170c6c4dfe27b2e97ce3
harry-0168 <harveers@ualberta.ca>
2024-10-14 01:34:48 -0600

    minor fix

--- nodeAdmin/views.py
+++ nodeAdmin/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse, HttpResponse, HttpResponseNotFound
-import requests
 from author.models import Author
 from django.conf import settings
 import json
