c5d316f14394c9740d753117c6bb3bf778a6e6ff
Haoze Sun <haoze7@ualberta.ca>
2024-10-14 00:26:28 -0600

    implemented CommonMark

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create_post, name="create"),
+    path('post/<uuid:post_id>/', views.view_post, name='view_post'),
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Post
 import base64
+import markdown
 
 # Create your views here.
 def index(request):
@@ -10,24 +11,35 @@ def create_post(request):
     if request.method == 'POST':
         title = request.POST['title']
         description = request.POST['description']
-        contentType = request.POST['content_type']
+        content_type = request.POST['content_type']
         visibility = request.POST['visibility']
         content = request.POST.get('content', '')
         image = request.FILES.get('img')
 
-        if contentType.startswith('image/') and image:
+        if content_type.startswith('image/') and image:
             # Read the image file and encode it as base64
             image_data = image.read()
             encoded_image = base64.b64encode(image_data).decode('utf-8')
             content = f"data:{image.content_type};base64,{encoded_image}"
+        
         post = Post(
             title=title,
             description=description,
-            content_type=contentType,
+            content_type=content_type,
             content=content,
             visibility=visibility,
             # author=request.user.author,
         )
-        post.save() 
+        post.save()
         #TO-DO change redirection
-        return redirect('index')
+        return redirect('view_post', post_id = post.id)
+    return render(request, 'posts/createPost.html') #If request method not POST, redirect to the createPost page
+
+
+def view_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    is_image = post.content_type.startswith('image/')
+    if post.content_type == 'text/markdown':
+        post.content = markdown.markdown(post.content)
+    
+    return render(request, 'posts/viewPost.html', {'post': post, 'is_image': is_image})
