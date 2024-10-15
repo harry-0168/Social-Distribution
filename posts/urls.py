e5aa211b5fe495c88a89800761dc710325bbc3b4
sharon2d24911 <sharon2d24911@gmail.com>
2024-10-15 15:06:42 -0600

    half way(haven't integrated api)

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create_post, name="create"),
+    path('editpost/', views.edit_post, name='edit_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .models import Post
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post, Author
 import base64
 
 # Create your views here.
@@ -14,7 +14,7 @@ def create_post(request):
         visibility = request.POST['visibility']
         content = request.POST.get('content', '')
         image = request.FILES.get('img')
-
+        author = get_object_or_404(Author, id=1)
         if contentType.startswith('image/') and image:
             # Read the image file and encode it as base64
             image_data = image.read()
@@ -26,8 +26,22 @@ def create_post(request):
             content_type=contentType,
             content=content,
             visibility=visibility,
-            # author=request.user.author,
+            author=author,
         )
         post.save() 
         #TO-DO change redirection
         return redirect('index')
+    
+def edit_post(request):
+    post = get_object_or_404(Post, id='dd8e3def6fd54d79ab860bea621bb9a6')
+
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.description = request.POST.get('description')
+        post.visibility = request.POST.get('visibility')
+        post.content_type = request.POST.get('content_type')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('post_list')  # Redirect to post list or detail page
+
+    return render(request, 'posts/editPost.html', {'post': post})
