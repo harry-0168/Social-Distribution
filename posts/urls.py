e87ae87e48778f9e302377c0057bfc09fcda6857
KhymNad <87257632+KhymNad@users.noreply.github.com>
2024-10-17 16:47:08 -0600

    added delete functionality, only Admins can see deleted posts

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("<uuid:id>/viewPost/", views.view_post, name="view"), # path to the specific post
     path("<uuid:id>/viewPost/likes/", views.view_postLikes, name="likes"), # path to posts view likes
     path("<uuid:id>/like/", views.like_post, name="like_post"),
+    path('<uuid:id>/delete/', views.delete_post, name='delete_post'),
 ]
