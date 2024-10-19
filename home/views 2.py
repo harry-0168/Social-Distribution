706077510c8b0f7286c017d93ccc36c2b4e4803f
KhymNad <87257632+KhymNad@users.noreply.github.com>
2024-10-19 17:48:41 -0600

    Revert "update to current version of development"
    This reverts commit e06c628156aa897f37c8315f1dad2dd54b712ce9.

--- home/views 2.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from posts.models import Post
-
-def home_page(request):
-    posts = Post.objects.filter(visibility='public')
-    return render(request, 'home/home_page.html', {'posts': posts})
--- nodeAdmin/apps 2.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NodeadminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nodeAdmin'
