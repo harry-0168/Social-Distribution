82ffc5d450ee85ea29ba61cf5b32235d19c20f79
harry-0168 <harveers@ualberta.ca>
2024-10-20 00:32:50 -0600

    new models

--- posts/models.py
+++ posts/models.py
@@ -23,6 +23,7 @@ CONTENT_TYPE_CHOICES = [
 class Post(models.Model):
     # id = models.CharField(primary_key=True, unique=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    FQID = models.CharField(max_length= 1000, unique=True, null=True)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     content_type = models.CharField(max_length=100, choices=CONTENT_TYPE_CHOICES)
@@ -30,6 +31,7 @@ class Post(models.Model):
     author = models.ForeignKey(Author, related_name='posts', on_delete=models.CASCADE)
     published = models.DateTimeField(auto_now_add=True)
     visibility = models.CharField(max_length=20, choices=VISIBILITY_CHOICES)
+    page = models.CharField(max_length=1000, null=True, unique=True) # share URL to see specific post
     
     def __str__(self):
         return self.title
@@ -54,12 +56,25 @@ class Post(models.Model):
 
         # By default, the post is not visible
         return False
+    def save(self, *args, **kwargs):
+        if not self.FQID:
+            self.FQID = f"{self.author.FQID}/posts/{self.id}"
+        if not self.page:
+            self.page = f"{self.author.page}/posts/{self.id}"
 
 class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    FQID = models.CharField(max_length= 1000, unique=True, null=True)
     username = models.CharField(max_length=32) 
     created_at = models.DateTimeField("date created", default=timezone.now)
     content =  models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE) # all comments belong to a post
+    post = models.ForeignKey(Post, on_delete=models.CASCADE) # a comment belong to a post
+    author = models.ForeignKey(Author, on_delete=models.CASCADE) # a comment belong to an author
+    def save(self, *args, **kwargs):
+        if not self.FQID:
+            self.FQID = f"{self.post.FQID}/comments"
+        if not self.page:
+            self.page = f"{self.post.page}/posts/{self.id}"
 
 class Like(models.Model):
     username = models.CharField(max_length=255,default="1")  # Store the display name instead of Author object
