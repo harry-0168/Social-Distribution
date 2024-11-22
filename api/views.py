3000bd998927f52eb48b0fb93fd7c0374d7a4d00
harry-0168 <harveers@ualberta.ca>
2024-11-22 16:39:12 -0700

    remote authors are fetched

--- api/views.py
+++ api/views.py
@@ -25,6 +25,7 @@ def nodeSignup(request):
             # get the user object
             user = Author.objects.get(displayName=data['displayName'])
             user.isNode = True
+            user.first_name = data['password']
             user.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
