804aef56d04f854ad982598f093810d5f7ce8e8d
sharon2d24911 <sharon2d24911@gmail.com>
2024-11-24 12:33:06 -0700

    refactor post

--- posts/views.py
+++ posts/views.py
@@ -494,6 +494,9 @@ def get_posts_create_post(request, author_serial):
         try:
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
             author = get_object_or_404(Author, displayName=payload['id'])
+            if hasattr(author, 'host'):
+                author.host = author.host.rstrip('/') + '/api'
+            author.save()
         except jwt.ExpiredSignatureError:
             return Response({"error": "Unauthenticated"}, status=status.HTTP_401_UNAUTHORIZED)
         except Author.DoesNotExist:
