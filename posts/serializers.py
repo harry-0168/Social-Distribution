77395802f95693ada2ecffea4c5dcdd02deb75fe
sharon2d24911 <sharon2d24911@gmail.com>
2024-11-25 13:50:04 -0700

    add likes to post

--- posts/serializers.py
+++ posts/serializers.py
@@ -17,6 +17,7 @@ class LikeSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True) # So the response actually return the author object instead of just id
     comments = serializers.SerializerMethodField()
+    likes = serializers.SerializerMethodField()
     class Meta:
         model = Post
         fields = ['id','uuid','type','page', 'title', 'description', 'contentType', 'content', 'visibility', 'author', 'published', 'comments']
@@ -31,6 +32,18 @@ class PostSerializer(serializers.ModelSerializer):
             "size": 5,
             "count": obj.comments.count(),
             "src": CommentSerializer(comments_queryset, many=True).data,
+        }
+
+    def get_likes(self, obj):
+        likes_queryset = obj.likes.all().order_by('-published')[:5] 
+        return {
+            "type": "likes",
+            "page": f"{obj.id}",
+            "id": f"{obj.id}/likes",
+            "page_number": 1,
+            "size": 5,
+            "count": obj.likes.count(),
+            "src": LikeSerializer(likes_queryset, many=True).data,
         }
 
     def validate_title(self, value):
