a086a63591bc78f8da857d640441f1aaf02f879a
sharon2d24911 <sharon2d24911@gmail.com>
2024-11-24 20:21:49 -0700

    remove /api

--- inbox/views.py
+++ inbox/views.py
@@ -404,6 +404,8 @@ def forward_like_request(request):
                     "id": like.id,
                     "object": like.object
                 }
+                if object_author.host.endswith('/api/'):
+                    object_author.host = object_author.host.split('/api/')[0]
                 print("object_author.host: ", object_author.host)
                 node_author = Author.objects.filter(host=object_author.host, isNode=True).first()
                 if not node_author:
