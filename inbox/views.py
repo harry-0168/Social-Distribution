d6cd83f01a03c4d11a8e667127805e2ce2de9ccd
sharon2d24911 <sharon2d24911@gmail.com>
2024-11-24 23:44:26 -0700

    removed uuid from comment

--- inbox/views.py
+++ inbox/views.py
@@ -188,15 +188,17 @@ def inboxApi(request, object_author_serial):
             author = get_object_or_404(Author, id=author_id)
 
             # Check if the comment already exists based on UUID
-            comment_uuid = parsed_data['uuid']
-            if Comment.objects.filter(uuid=comment_uuid).exists():
+            comment_id = parsed_data['id']
+            print("comment_id: ", comment_id)
+            comment_uuid = parsed_data['id'].split('/')[-1]
+            if Comment.objects.filter(id=comment_id).exists():
                 return Response({"message": "Comment already exists"}, status=status.HTTP_200_OK)
 
             # forward the comment to the remote post_author's inbox
             post_author = Author.objects.get(id=post_author_id)
             Inbox(receiver=post_author, type='comment', FQIDorId=parsed_data['id'], received_at=timezone.now()).save()
 
-            comment = Comment(author=author, username=parsed_data['author']['displayName'], comment=parsed_data['comment'], published=parsed_data['published'], id=parsed_data['id'] , uuid=parsed_data['uuid'], post=post)
+            comment = Comment(author=author, username=parsed_data['author']['displayName'], comment=parsed_data['comment'], published=parsed_data['published'], id=parsed_data['id'], uuid=comment_uuid, post=post)
             print("\nComment: ", comment)
 
             host = request.get_host()
