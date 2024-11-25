5c778e3df562de89cc1eba140b27ee102541c52c
KhymNad <87257632+KhymNad@users.noreply.github.com>
2024-11-24 18:58:49 -0700

    fixing comment payload for inbox

--- inbox/views.py
+++ inbox/views.py
@@ -182,11 +182,14 @@ def inboxApi(request, object_author_serial):
             print("post.id: ", post.id)
             print("post.title: ", post.title)
 
+            author_id = parsed_data['author']['id']
+            author = get_object_or_404(Author, id=author_id)
+
             # forward the comment to the remote post_author's inbox
             post_author = Author.objects.get(id=post_author_id)
             Inbox(receiver=post_author, type='comment', FQIDorId=parsed_data['id'], received_at=timezone.now()).save()
 
-            comment = Comment(author=parsed_data['author']['id'], username=parsed_data['author']['displayName'], comment=parsed_data['comment'], published=parsed_data['published'], id=parsed_data['id'] , uuid=parsed_data['uuid'], post=post)
+            comment = Comment(author=author, username=parsed_data['author']['displayName'], comment=parsed_data['comment'], published=parsed_data['published'], id=parsed_data['id'] , uuid=parsed_data['uuid'], post=post)
             print("\nComment: ", comment)
             comment.save()
             print("comment saved")
